9916986f3784ff5e92b76eb0c6483b78240f6a1d
Yuvraj-Parade <work.yuvrajparade221@gmail.com>
2025-09-21 16:23:33 +0530

    solve the issue in app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 
 // Root route to prevent “Cannot GET /”
 app.get("/", (req, res) => {
-  res.send("Welcome to WanderWisp!");
+  res.render("/views/layouts/boilerplate.ejs");
 });
 
 // Main routers
