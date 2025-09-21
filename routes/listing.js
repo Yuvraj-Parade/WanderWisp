cb8e0c2fa08a0b4644e1bf008404f41cc6250536
Yuvraj-Parade <work.yuvrajparade221@gmail.com>
2025-09-21 18:45:48 +0530

    solve the issue in app.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
-const listingController = require("./controllers/listings.js");
+const listingController = require("../controllers/listings.js");
 const multer  = require('multer')
 const {storage} = require("../clouldConfig.js");
 const upload = multer({ storage })
@@ -32,7 +32,6 @@ router
 // router.get("/", wrapAsync(listingController.index));
 
 
- 
 //show route
 // router.get("/:id",wrapAsync(listingController.showListing));
 
